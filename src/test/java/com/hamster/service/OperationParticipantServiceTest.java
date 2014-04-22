f94d0a5cb4add6a04756be79fff94455fb480403
hamster1980 <79819721701@yandex,ru>
2014-04-22 22:15:24 +0400

    new test cases

---
+++ src/test/java/com/hamster/service/OperationParticipantServiceTest.java
@@ -0,0 +1,10 @@
+package com.hamster.service;
+
+public class OperationParticipantServiceTest {
+
+    /*
+     * addParticipant
+     * 1. operation in status STARTED can have not more then one participant
+     * 2. check that for author confirmation is not sent
+     */
+}
